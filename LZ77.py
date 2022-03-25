7d1298de3de71abd7f4dca8b6ced5e5007504c35
Jake Mehlman <89606961+jdm365@users.noreply.github.com>
2022-03-24 23:13:42 -0400

    Finished super basic LZ77 encoding logic.

--- LZ77.py
+++ LZ77.py
@@ -1,6 +1,3 @@
-
-
-
 def get_init_buffer(input_string):
     search_buffer = []
     for idx, element in enumerate(input_string):
@@ -14,38 +11,43 @@ def encode_once(search_buffer, look_ahead_buffer):
     score = 0
     matching = False
     matches = []
-    old_string = search_buffer + look_ahead_buffer
+    remove_indices = []
+    new_string = ''
     for i in range(len(look_ahead_buffer)):
         if search_buffer[idx % len(search_buffer)] == look_ahead_buffer[i]:
             score += 1
             idx += 1
             matching = True
+            remove_indices.append(i + len(search_buffer))
         elif matching:
             matches.append((look_ahead_buffer[i], score, i))
             idx = 0
             score = 0
             matching = False
-    return matches
+            new_string += look_ahead_buffer[i]
+        else:
+            new_string += look_ahead_buffer[i]
+    return matches, new_string
             
 
 def encode_file(input_string):
-    search_buffer = get_init_buffer(input_string)
-    start_idx = len(search_buffer)
-    file_length = len(input_string)
+    matches = []
+    while True:
+        search_buffer = get_init_buffer(input_string)
+        start_idx = len(search_buffer)
+        file_length = len(input_string)
 
-    if start_idx == file_length:
-        raise ValueError('File is maximally LZ77 compressed!')
-
-    look_ahead_buffer = input_string[start_idx:file_length]
-
-    score = 0
-    for idx, element in enumerate(look_ahead_buffer):
-        if element == search_buffer[idx]:
-            score += 1
+        if start_idx == file_length:
+            print('File is maximally LZ77 compressed!')
+            break
+        look_ahead_buffer = input_string[start_idx:file_length]
+        
+        new_matches, input_string = encode_once(search_buffer, look_ahead_buffer)
+        matches += new_matches
+    return matches, input_string
 
 
 string = 'asdfa'
 search_buffer = 'abcd'
 look_ahead_buffer = 'apples and oranges are great and good and delicious abcd!'
-print(encode_once(search_buffer, look_ahead_buffer))
-#encode(string)
+print(encode_file(look_ahead_buffer))
